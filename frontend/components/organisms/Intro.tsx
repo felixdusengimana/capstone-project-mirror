2c117667abddfa214c82b8e3e38bce2eccc30b2b
felixdusengimana <phelixdusengimana@gmail.com>
2026-02-08 21:18:19 +0200

    feat: image still have issues

--- frontend/components/organisms/Intro.tsx
+++ frontend/components/organisms/Intro.tsx
@@ -9,7 +9,7 @@ export default function Intro() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 page-padding">
       <div className="mt-40 max-w-[722px]">
-        <Headline className="text-[64px] leading-[79.47px]">
+        <Headline className="heading leading-[79.47px]">
           Search and gift your favorite creators
         </Headline>
         <p className="text-xl text-[#8A8A8B] max-w-[535px] mt-5 mb-10">
@@ -28,6 +28,7 @@ export default function Intro() {
         </div>
       </div>
       <div className="h-full w-full hidden lg:block">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
         <img src="./home-bg.png" alt="" className="w-full h-full" />
       </div>
     </div>
