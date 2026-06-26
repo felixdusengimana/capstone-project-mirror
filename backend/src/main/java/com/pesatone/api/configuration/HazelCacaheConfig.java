4639aad2c865b6e66f9a38a5a846f6b1df822510
felixdusengimana <phelixdusengimana@gmail.com>
2026-06-26 21:00:59 +0200

    networking config for jvm

--- backend/src/main/java/com/pesatone/api/configuration/HazelCacaheConfig.java
+++ backend/src/main/java/com/pesatone/api/configuration/HazelCacaheConfig.java
@@ -1,13 +1,16 @@
 package com.pesatone.api.configuration;
 
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.hazelcast.config.Config;
 import com.hazelcast.config.MapConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.spring.cache.HazelcastCacheManager;
 
 @Configuration
 @EnableCaching
@@ -29,6 +32,7 @@ public class HazelCacaheConfig {
 	}
 
 	@Bean
+	@Primary
 	public HazelcastInstance hazelcastInstance(Config config) {
 		return Hazelcast.newHazelcastInstance(config);
 	}
